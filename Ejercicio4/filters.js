b4a985852942120798aab63c8b1cf4b97efb6fec
Carmen <uo264637@uniovi.es>
2023-05-22 16:12:16 +0200

    added filters and details page

---
+++ Ejercicio4/filters.js
@@ -0,0 +1,163 @@
+// Creación del mapa
+const map = L.map('map').setView([54.5970, -5.9300], 11);
+var markersLayer = L.layerGroup().addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+
+// JSON de tipos de especie
+const speciesData = {
+    "Alder":"Q25239",
+    "Amelanchier":"Q156957",
+    "Apple":"Q18674606",
+    "Ash":"Q156907",
+    "Beech":"Q59779138",
+    "Birch":"Q25243",
+    "Cedar":"Q128550",
+    "Cherry":"Q62474322",
+    "Chestnut":"Q129324",
+    "Cotoneaster":"Q5538",
+    "Crab Apple":"Q47161",
+    "Cypress":"Q146911",
+    "Elder":"Q22701",
+    "Elm":"Q59779164",
+    "Eucalyptus":"Q45669",
+    "False Acaia":"Q157417",
+    "Fir":"Q25350",
+    "Gingko":"Q43284",
+    "Handkerchief":"Q18348215",
+    "Hawthorn":"Q132557",
+    "Hazel":"Q124969",
+    "Holly":"Q117085",
+    "Hornbeam":"Q59779148",
+    "Irish Yew":"Q110767650",
+    "Juniper":"Q26325",
+    "Laburnum":"Q147184",
+    "Larch":"Q61996280",
+    "Lawson Cypress":"Q161360",
+    "Lime":"Q45417383",
+    "Liquidamber":"Q183543",
+    "Magnolia":"Q157017",
+    "Maple":"Q42292",
+    "Oak":"Q12004",
+    "Pear":"Q434",
+    "Pine":"Q12024",
+    "Plane":"Q157739",
+    "Plum":"Q6401215",
+    "Poplar":"Q30456678",
+    "Rauli":"Q1317892",
+    "Redwood":"Q1975652",
+    "Rowan":"Q157964",
+    "Spruce":"Q26782",
+    "Sweet Chestnut":"Q22699",
+    "Tree Of Heaven":"Q159570",
+    "Tulip":"Q158783",
+    "Walnut":"Q46871",
+    "Western Hemlock":"Q1144409",
+    "Willow":"Q36050",
+    "Yew":"Q27355",
+    "Yucca":"Q156317"
+  }
+  
+  const selectedValues = [];
+  const filterList = document.getElementById('filter-list');
+  
+
+  for (const species in speciesData) {
+    const listItem = document.createElement('li');
+    
+    // Crear el checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.value = speciesData[species];
+    if (species == "Gingko"){
+        checkbox.checked = true;
+        selectedValues.push(speciesData[species]);
+        query(selectedValues);
+    }
+    checkbox.autocomplete='off';
+
+    checkbox.addEventListener('change', () => { // Añade o elimina el filtro de la lista y llama a la consulta
+        const value = checkbox.value;
+        
+        if (checkbox.checked) {
+          selectedValues.push(value);
+        } else {
+          const index = selectedValues.indexOf(value);
+          if (index > -1) {
+            selectedValues.splice(index, 1);
+          }
+        }
+        query(selectedValues);
+      });
+    
+    // Crear la etiqueta del checkbox
+    const label = document.createElement('label');
+    label.textContent = " "+ species;
+    label.setAttribute('for', speciesData[species]);
+    
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    filterList.appendChild(listItem);
+  }
+  
+  //Obtiene los árboles de los filtros marcados (el límite es 1000 porque si no se ralentiza mucho el navegador)
+  function query(selectedValues) {
+    const valuesStr = selectedValues.map(value => `wd:${value}`).join(', ');
+
+    fetch('http://156.35.98.70:3030/trees_ds/sparql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'query=' + encodeURIComponent(`
+                PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+                PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+                PREFIX wd: <http://www.wikidata.org/entity/>
+
+                SELECT ?tree ?latitude ?longitude 
+                WHERE {
+                    ?tree geo:lat ?latitude ;
+                        geo:long ?longitude .
+                    ?tree wdt:P31 ?speciesType .
+                    FILTER (?speciesType IN (${valuesStr}))
+                }
+                LIMIT 1000
+        `),
+    })
+  .then(response => response.json())
+  .then(data => {
+    markersLayer.clearLayers(); // Elimina marcas anteriores del mapa
+    // Cambia los marcadores por defecto del mapa por iconos de árbol
+    const treeIcon = L.icon({
+        iconUrl: './resources/tree-icon.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+        iconSize: [35, 40],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+    const results = data.results.bindings;
+    for (const binding of results) { // Crea una marca para cada árbol y le añade un popup con un enlace a más detalles
+        const treeURI = binding.tree.value;
+        const latitude = parseFloat(binding.latitude.value);
+        const longitude = parseFloat(binding.longitude.value);
+
+        const marker = L.marker([latitude, longitude], { icon: treeIcon }).addTo(markersLayer);
+
+        const content = `
+          <div>
+            <h3>${treeURI}</h3>
+            <p>Latitude: ${latitude}</p>
+            <p>Latitude: ${longitude}</p>
+            <a href=tree-details.html?tree=${treeURI}>View Details</a>
+          </div>
+        `;
+
+        marker.bindPopup(content);
+    }
+  })
+  .catch(error => console.error('Error:', error));
+  }
---
+++ Ejercicio4/tree-details.js
@@ -0,0 +1,120 @@
+// Obtener los parámetros de consulta de la URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// Obtener los valores de los parámetros
+const treeURI = urlParams.get('tree');
+const tree = treeURI.split("/").slice(-1)[0];
+console.log(tree);
+
+fetch('http://156.35.98.70:3030/trees_ds/sparql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: 'query=' + encodeURIComponent(`
+        PREFIX schema: <http://schema.org/>
+        PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+        PREFIX dbr: <http://dbpedia.org/resource/>
+        PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> 
+        PREFIX qudt: <https://qudt.org/2.1/vocab/unit> 
+        PREFIX tree: <http://example.org/tree/> 
+        PREFIX wd: <http://www.wikidata.org/entity/> 
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT ?condition ?description ?diameter ?spreadRadius ?height ?treeSurround ?type ?units ?vigour ?latitude ?longitude ?label ?age ?species ?tag ?speciesType (SAMPLE(?image) AS ?sampleImage)
+        WHERE {
+            tree:${tree} tree:condition ?condition  ;
+                         tree:description ?description ;
+                         tree:diameter ?diameter ;
+                         tree:spreadRadius ?spreadRadius ;
+                         schema:height ?height ;
+                         tree:treeSurround ?treeSurround ;
+                         tree:type ?type ;
+                         tree:units ?units ;
+                         tree:vigour ?vigour ;
+                         geo:lat ?latitude ;
+                         geo:long ?longitude .
+            OPTIONAL {tree:${tree} tree:age ?age}
+            OPTIONAL {tree:${tree} a ?species}
+            OPTIONAL {tree:${tree} tree:tag ?tag}
+            OPTIONAL {tree:${tree} wdt:P31 ?speciesType
+                SERVICE <https://query.wikidata.org/sparql> {
+                    ?speciesType rdfs:label ?label
+                    FILTER(LANG(?label) = "en")
+                    ?speciesType wdt:P18 ?image
+                }
+            }
+        }
+        GROUP BY ?condition ?description ?diameter ?spreadRadius ?height ?treeSurround ?type ?units ?vigour ?latitude ?longitude ?label ?age ?species ?tag ?speciesType
+`),
+})
+  .then(response => response.json())
+  .then(data => {
+
+    const results = data.results.bindings;
+    for (const binding of results) {
+        const condition = binding.condition.value;
+        const description = binding.description.value;
+        const diameter = binding.diameter.value;
+        const spreadRadius = binding.spreadRadius.value;
+        const height = binding.height.value;
+        const treeSurround = binding.treeSurround.value;
+        const type = binding.type.value;
+        const units = binding.units.value.split("https://qudt.org/2.1/vocab/unit")[1];
+        const vigour = binding.vigour.value;
+        const latitude = parseFloat(binding.latitude.value);
+        const longitude = parseFloat(binding.longitude.value);
+
+        var age = "";
+        var species = "";
+        var tag ="";
+        var speciesType = "";
+        var sampleImage = "";
+        var treeLabel = "";
+
+
+        if (binding.age) {
+            age = binding.age.value;
+        }
+        
+        if (binding.species) {
+            species = binding.species.value;
+        }
+
+        if (binding.tag) {
+            tag = binding.tag.value;
+        }
+
+        if (binding.speciesType) {
+            speciesType = binding.speciesType.value;
+            sampleImage =binding.sampleImage.value;
+            treeLabel = binding.label.value;
+        }
+
+        // Mostrar los detalles del árbol en la página
+        const treeDetailsElement = document.getElementById('tree-details');
+        treeDetailsElement.innerHTML = `
+            <hgroup>
+                <h2>${tree}</h2>
+                <p><strong>Uri:</strong> ${treeURI}</p>
+            </hgroup>
+            <img src=${sampleImage} alt="${treeLabel}" onerror="this.onerror=null; this.alt='Image not found'">
+            <p><strong>Species Type:</strong> ${treeLabel} <a href="${speciesType}">${speciesType}</a></p>
+            <p><strong>Species:</strong> ${species}</p>
+            <p><strong>Age:</strong> ${age}</p>
+            <p><strong>Condition:</strong> ${condition}</p>
+            <p><strong>Vigour:</strong> ${vigour}</p>
+            <p><strong>Tree Surround:</strong> ${treeSurround}</p>
+            <p><strong>Description:</strong> ${description}</p>
+            <p><strong>Diameter:</strong> ${diameter} ${units}</p>
+            <p><strong>Spread Radius:</strong> ${spreadRadius} ${units}</p>
+            <p><strong>Height:</strong> ${height} ${units}</p>    
+            <p><strong>Type:</strong> ${type}</p>
+            <p><strong>Latitud:</strong> ${latitude}</p>
+            <p><strong>Longitud:</strong> ${longitude}</p>
+        `;
+    }
+  })
+  .catch(error => console.error('Error:', error));
